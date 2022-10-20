6ec8157cbe33e562af1bc6ddb9bcc011d62a1c93
Jake Gowler <jakecgowler@gmail.com>
2022-10-20 12:03:27 +0100

    Emergency save due to potential crash.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        #
+    return render(request, 'accounts/signup.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    # TODO need to route to homepage
+    return render(request, 'accounts/signup.html')
